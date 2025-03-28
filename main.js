ad1301d47e264cc6e08a1decf4a3187c83f08228
P4ciuf <p4ciuf@gmail.com>
2025-03-28 19:07:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,12 +10,10 @@ const ffmpeg = require('ffmpeg-static');
 const fs = require('fs');
 const path = require('path');
 
-// Configura il tuo token Discord e l'ID del canale vocale
 const TOKEN = 'MTM1NDUyNzgwNDQxODU2MDA1Mw.GFh9q_._Or-heoU8TFcoRJqG7Z_YjwQltre-Y2X-fxVt0';
 const VOICE_CHANNEL_ID = '1213582269936308265';
 const GUILD_ID = '1212788183079714846';
 
-// Percorso del file audio
 const AUDIO_FILE_PATH = path.join(__dirname, 'audio', 'song.mp3');
 
 const client = new Client({
@@ -28,7 +26,6 @@ const client = new Client({
 client.once('ready', () => {
     console.log(`Bot avviato: ${client.user.tag}`);
     
-    // Connetti al canale vocale
     const channel = client.channels.cache.get(VOICE_CHANNEL_ID);
     if (channel) {
         try {
@@ -37,24 +34,17 @@ client.once('ready', () => {
                 guildId: GUILD_ID,
                 adapterCreator: channel.guild.voiceAdapterCreator
             });
-
-            // Crea il lettore audio
             const player = createAudioPlayer();
             
-            // Crea la risorsa audio
             const resource = createAudioResource(AUDIO_FILE_PATH, {
                 inputType: StreamType.Arbitrary
             });
 
-            // Collega il player al canale
             connection.subscribe(player);
 
-            // Avvia la riproduzione
             player.play(resource);
 
-            // Gestisci il loop
             player.on(AudioPlayerStatus.Idle, () => {
-                // Ricrea la risorsa audio e riproduci
                 const newResource = createAudioResource(AUDIO_FILE_PATH, {
                     inputType: StreamType.Arbitrary
                 });
